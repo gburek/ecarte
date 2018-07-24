ac7313dbc93f51f2eea44c7152756813ead44be3
greg Burek <burekgrz@gmail.com>
2018-07-24 15:33:28 +0000

    Added Restaurant model.

--- api/api/models.py
+++ api/api/models.py
@@ -1,5 +1,6 @@
 import enum
-from sqlalchemy import Table, Column, Integer, String, Enum, DateTime, Boolean, ForeignKey
+from sqlalchemy import Table, Column, Integer, Float, String, Unicode, Enum, DateTime, Boolean, ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.hash import sha256_crypt
@@ -28,13 +29,18 @@ account_roles = Table('account_role',
                       Column('username', String, ForeignKey('account.username')),
                       Column('role_id', Integer, ForeignKey('role.id')))
 
+account_restaurants = Table('account_restaurant',
+	                        DBModel.metadata,
+	                        Column('username', ForeignKey('account.username')),
+	                        Column('restaurant_id', ForeignKey('restaurant.id')))
+
 
 class Account(DBModel):
 	__tablename__ = 'account'
 
 	id = Column(Integer, primary_key=True)
 	username = Column(String, unique=True, index=True)
-	fullName = Column(String)
+	fullName = Column(Unicode)
 	email = Column(String, unique=True, index=True)
 	login_method = Column(Enum(AuthType), name='auth_type', nullable=False, default=AuthType.local)
 	pwd = Column(String)
@@ -57,9 +63,29 @@ class Account(DBModel):
 		return self._is_admin
 
 
-class Business(DBModel):
-	__tablename__ = 'business'
+# All language codes are ISO 639-1
+# Anything named *_intln contains translation(s) to secondary language(s) stored as JSON {lang-code: value}
+
+class Restaurant(DBModel):
+	__tablename__ = 'restaurant'
 
 	id = Column(Integer, primary_key=True)
-	name = Column(String, index=True)
-	
+	active = Column(Boolean, default=True)
+	name = Column(Unicode, index=True)
+	primary_lang = Column(String(2), default='en')
+	# list of language codes
+	extra_langs = Column(JSONB)
+	description = Column(Unicode)
+	# {'ko': 'Korean translation'}
+	description_intl = Column(JSONB)
+	keywords = Column(Unicode)
+	address1 = Column(Unicode(100))
+	address2 = Column(Unicode(100))
+	city = Column(Unicode(100), index=True)
+	postal_code = Column(String(12))
+	province_code = Column(String(2))
+	country = Column(Unicode(20))
+	lat = Column(Float, index=True)
+	long = Column(Float, index=True)
+	order_phone = Column(String(20))
+	managing_users = relationship('Account', secondary=account_restaurants, lazy='subquery')
